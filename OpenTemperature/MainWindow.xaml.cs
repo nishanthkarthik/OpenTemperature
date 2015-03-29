ef04318379347633aef1376135466d4cccad09aa
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-03-29 15:33:51 +0530

    Added sensor value reading on code

--- OpenTemperature/MainWindow.xaml.cs
+++ OpenTemperature/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using OpenHardwareMonitor.Hardware;
 
 namespace OpenTemperature
 {
@@ -20,9 +11,58 @@ namespace OpenTemperature
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Computer _computer;
+        private bool _isComputerOpen;
+        private DispatcherTimer _dispatcherTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!(_dispatcherTimer == null || _dispatcherTimer.IsEnabled))
+            {
+                _dispatcherTimer.Stop();
+                _dispatcherTimer = null;
+            }
+            if (_isComputerOpen)
+                _computer.Close();
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _computer = new Computer
+            {
+                CPUEnabled = true,
+                FanControllerEnabled = true,
+                GPUEnabled = true,
+                HDDEnabled = true,
+                MainboardEnabled = true,
+                RAMEnabled = true
+            };
+            try
+            {
+                _computer.Open();
+                _isComputerOpen = true;
+                UpdateSensorValue();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                throw;
+            }
+        }
+
+        void UpdateSensorValue()
+        {
+            foreach (IHardware hardware in _computer.Hardware)
+            {
+                ;
+            }
         }
     }
 }
